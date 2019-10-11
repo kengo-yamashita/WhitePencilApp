b6d4b2e7d9761ed17c824a2d83256a5c154ded94
unknown <yamashita-kenngo@outlook.jp>
2019-10-11 13:10:31 +0900

    マップの表示

--- MVP/src/app/tab2/tab2.page.ts
+++ MVP/src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, latLng, tileLayer, Layer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  map: Map;
 
-  constructor() {}
+  ionViewDidEnter() { this.leafletMap()};
 
+  leafletMap() {
+    // 地図の見た目を設定
+    this.map = new Map('mapId').setView([34.395483, 132.453592], 17);
+    tileLayer('http://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {
+      attribution: 'Code for Hiroshima',
+    }).addTo(this.map);
+
+    // マーカー
+    marker([34.393454, 132.448778]).addTo(this.map)
+        .bindPopup('ハチドリ舎')
+        .openPopup();
+
+
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 }
